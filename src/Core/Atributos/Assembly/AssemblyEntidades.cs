0ae4d37dc36300618cfff39ab3cb9698008f9f25
Rubens Cordeiro <zyoncore@hotmail.com>
2023-07-20 21:02:41 -0300

    separados os atributos assembly

--- src/Core/Atributos/Assembly/AssemblyEntidades.cs
+++ src/Core/Atributos/Assembly/AssemblyEntidades.cs
@@ -8,11 +8,4 @@ namespace Snebur.Dominio.Atributos
     {
 
     }
-
-    [IgnorarAtributoTS]
-    [AttributeUsage(AttributeTargets.Assembly)]
-    public class NaoValidarNomeTabelaAttribute : Attribute
-    {
-
-    }
 }
