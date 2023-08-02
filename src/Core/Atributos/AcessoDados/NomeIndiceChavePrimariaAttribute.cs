d54ce9ee828e2ce576a3f13700f43eba12b4e2ba
Rubens Cordeiro <zyoncore@hotmail.com>
2023-08-02 18:34:06 -0300

    adicionado atributo NomeIndiceChavePrimariaAttribute

---
+++ src/Core/Atributos/AcessoDados/NomeIndiceChavePrimariaAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Snebur.Dominio.Atributos
+{
+    [IgnorarAtributoTS]
+    [AttributeUsage(AttributeTargets.Class)]
+    public class NomeIndiceChavePrimariaAttribute : Attribute
+    {
+        public string Nome { get; set; }
+        public NomeIndiceChavePrimariaAttribute(string nome)
+        {
+            this.Nome = nome;
+        }
+
+    }
+}
