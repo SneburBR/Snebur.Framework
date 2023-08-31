0a546aab0f94836ca1f9cea482b5d9cda8c80a05
Rubens Cordeiro <zyoncore@hotmail.com>
2023-08-31 12:33:31 -0300

    adicionado SqlBuilder2008

---
+++ src/AcessoDados/src/AcessoDados.Servidor/Consulta/Entidade/SqlBuilder2008.cs
@@ -0,0 +1,50 @@
+﻿namespace Snebur.AcessoDados.Mapeamento
+{
+    internal class SqlBuilder2008 : BaseSqlBuilder
+    {
+        internal SqlBuilder2008(BaseMapeamentoEntidade mapeamentoConsulta) : base(mapeamentoConsulta) { }
+
+        internal override string MontarSql(BaseFiltroMapeamento filtroMapeamento, 
+                                           string sqlCampos, 
+                                           bool isIncluirOrdenacaoPaginacao, 
+                                           bool isRelacaoFilhos)
+        {
+            if (!isIncluirOrdenacaoPaginacao)
+            {
+                return base.MontarSql(filtroMapeamento, 
+                                      sqlCampos, 
+                                      isIncluirOrdenacaoPaginacao, 
+                                      isRelacaoFilhos);
+            }
+
+            if(this.Skip> 0)
+            {
+                return this.MontarSqlPaginacao(filtroMapeamento,
+                                               sqlCampos,
+                                               isRelacaoFilhos);
+            }
+
+            var sqlConsulta= this.RetornarSqlConsulta(filtroMapeamento, 
+                                                      isIncluirOrdenacaoPaginacao, 
+                                                      isRelacaoFilhos);
+
+            return $"SELECT TOP {this.Take} {sqlCampos} FROM {sqlConsulta}";
+        }
+
+        private string MontarSqlPaginacao(BaseFiltroMapeamento filtroMapeamento, 
+                                          string sqlCampos,
+                                          bool isRelacaoFilhos)
+        {
+            var sqlOrdenacao = this.RetornarSqlOrdenacao(filtroMapeamento);
+            var sqlConsulta = this.RetornarSqlConsulta(filtroMapeamento, false, isRelacaoFilhos);
+            var sb = new StringBuilderSql();
+            sb.AppendLine("WITH ConsultaPaginada AS ");
+            sb.AppendLine("(");
+            sb.AppendLine($"    SELECT ROW_NUMBER() OVER ({sqlOrdenacao}) AS RowNumber, {sqlCampos}");
+            sb.AppendLine($"           FROM {sqlConsulta}");
+            sb.AppendLine(")");
+            sb.AppendLine($"SELECT * FROM ConsultaPaginada WHERE RowNumber BETWEEN {this.Skip + 1} AND {this.Skip + this.Take}");
+            return sb.ToString();
+        }
+    }
+}
