3ff1042149817c10f10e2045a21704ddf704a285
Rubens Cordeiro <zyoncore@hotmail.com>
2023-09-06 21:23:28 -0300

    adicionado ValidacaoGuidAttribute

---
+++ src/Core/Atributos/Validacao/ValidacaoGuidAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Snebur.Utilidade;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Snebur.Dominio.Atributos
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ValidacaoGuidAttribute : BaseAtributoValidacao, IAtributoValidacao
+    {
+        [MensagemValidacao]
+        public static string MensagemValidacao { get; set; } = "O campo {0} é invalido.";
+
+        public ValidacaoGuidAttribute()
+        {
+        }
+
+        #region IAtributoValidacao
+
+        public override bool IsValido(PropertyInfo propriedade, object paiPropriedade, object valorPropriedade)
+        {
+            if (!ValidacaoUtil.IsDefinido(valorPropriedade))
+            {
+                return !ValidacaoUtil.IsPropriedadeRequerida(propriedade);
+            }
+            var value = Convert.ToString(valorPropriedade);
+            return ValidacaoUtil.IsGuid(value);
+        }
+
+        [Display]
+        public override string RetornarMensagemValidacao(PropertyInfo propriedade, object paiPropriedade, object valorPropriedade)
+        {
+            var rotulo = ReflexaoUtil.RetornarRotulo(propriedade);
+            return String.Format(MensagemValidacao, rotulo);
+        }
+        #endregion
+    }
+}
+
