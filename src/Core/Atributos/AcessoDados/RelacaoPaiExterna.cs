5d92f79f5c4fafe8272cfc0def8652aca3bd1c61
Rubens Cordeiro <zyoncore@hotmail.com>
2023-09-10 18:33:10 -0300

    adicionado RelacaoPaiExterna

---
+++ src/Core/Atributos/AcessoDados/RelacaoPaiExterna.cs
@@ -0,0 +1,15 @@
+﻿namespace Snebur.Dominio.Atributos
+{
+    public class RelacaoPaiExterna : NaoMapearAttribute, IIgnorarAlerta
+    {
+        public bool IgnorarAlerta => true;
+        public string NomePropriedadeChaveEstrangeira { get; set; }
+        public string NomeContextoDadosExterno { get; set; }
+
+        public RelacaoPaiExterna(string nomeContextoDadosExtexto, string nomePropriedadeChaveEstrangeira)  
+        {
+            this.NomePropriedadeChaveEstrangeira = nomePropriedadeChaveEstrangeira;
+            this.NomeContextoDadosExterno = nomeContextoDadosExtexto;
+        }
+    }
+}
