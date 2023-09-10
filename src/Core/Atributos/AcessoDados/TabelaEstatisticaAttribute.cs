d04ab2a467026f11668afb5a136c6067bfbecc6d
Rubens Cordeiro <zyoncore@hotmail.com>
2023-09-10 18:33:51 -0300

    adicionado TabelaEstatisticaAttribute

---
+++ src/Core/Atributos/AcessoDados/TabelaEstatisticaAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Snebur.Dominio.Atributos
+{
+    [IgnorarAtributoTS]
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TabelaEstatisticaAttribute : TabelaAttribute
+    {
+        public const string SCHEMA = "estatistica";
+
+        public TabelaEstatisticaAttribute(string nomeTabela) : base(nomeTabela, SCHEMA)
+        {
+        }
+    }
+}
