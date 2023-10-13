0d6330ffa09c798d9ba1bc6e74a66b8204ada4ab
Rubens Cordeiro <zyoncore@hotmail.com>
2023-10-12 21:13:41 -0300

    adicionado HtmlUtil

---
+++ src/Core/Utilidade/HtmlUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Snebur.Utilidade
+{
+    public static class HtmlUtil
+    {
+        public static string RetornarTexto(string html)
+        {
+            return html.ReplaceRegex(@"<br\s*/?>", "\r\n")
+                       .ReplaceRegex(@"<p\s*/?>", "\r\n")
+                       .ReplaceRegex(@"<[^>]+>", string.Empty).
+                        ReplaceRegex(@"&[A-Za-z][A-Za-z0-9]{1,4};", string.Empty);
+        }
+
+    }
+}
