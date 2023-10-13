e82198254ecb11f04f6efc4dabbec692d37aa527
Rubens Cordeiro <zyoncore@hotmail.com>
2023-10-13 12:34:53 -0300

    adicionado StreamProgressEventArgs

---
+++ src/Core/Utilidade/StreamProgressEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Snebur.Utilidade
+{
+    public class StreamProgressEventArgs : EventArgs
+    {
+        public long TotalBytesRecebitos { get; set; }
+        public long TotalBytes { get; set; }
+
+        public double Progresso
+        {
+            get
+            {
+                if (this.TotalBytes == 0)
+                {
+                    return 0;
+                }
+                return (double)this.TotalBytesRecebitos / this.TotalBytes;
+            }
+        }
+
+        public StreamProgressEventArgs(long totalBytesRecebitos, long totalBytes)
+        {
+            this.TotalBytesRecebitos = totalBytesRecebitos;
+            this.TotalBytes = totalBytes;
+        }
+
+    }
+}
