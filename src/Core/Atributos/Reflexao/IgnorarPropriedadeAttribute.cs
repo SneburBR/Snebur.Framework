fbf344249bde5b05410df7fce481fa7ce13968f1
Rubens Cordeiro <zyoncore@hotmail.com>
2023-10-16 16:08:25 -0300

    adicionado IgnorarPropriedadeAttribute

---
+++ src/Core/Atributos/Reflexao/IgnorarPropriedadeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Snebur.Dominio.Atributos
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class IgnorarPropriedadeAttribute : XmlIgnoreAttribute
+    {
+    }
+
+
+}
