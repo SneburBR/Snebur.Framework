cfaec3fc20ab56490daa6346554b3f395b6a2fb8
Rubens Cordeiro <zyoncore@hotmail.com>
2024-04-04 21:05:05 -0300

    adicionado IsIdType e IsNumericType na extensao do Type

--- src/Core/Extensao/TypeExtensao.cs
+++ src/Core/Extensao/TypeExtensao.cs
@@ -1,10 +1,48 @@
-﻿namespace System
+﻿using System.Collections.Generic;
+
+namespace System
 {
     public static class TypeExtensao
     {
+        private static HashSet<Type> NumericTypes = new HashSet<Type> {
+            typeof(byte),
+            typeof(sbyte),
+            typeof(short),
+            typeof(ushort),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(float),
+            typeof(double),
+            typeof(decimal)
+        };
+
+        private static HashSet<Type> IdTypes = new HashSet<Type> {
+            typeof(byte),
+            typeof(short),
+            typeof(ushort),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+        };
+
         public static string RetornarCaminhoTipo(this Type tipo)
         {
             return $"{tipo.Namespace}.{tipo.Name}";
+        }
+
+        internal static bool IsNumericType(this Type type)
+        {
+            return NumericTypes.Contains(type) ||
+                   NumericTypes.Contains(Nullable.GetUnderlyingType(type));
+        }
+
+        internal static bool IsIdType(this Type type)
+        {
+            return IdTypes.Contains(type) ||
+                   IdTypes.Contains(Nullable.GetUnderlyingType(type));
         }
     }
 }
