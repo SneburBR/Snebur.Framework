9d355483aab6024738ab64d581342b1869b19c09
Rubens Cordeiro <zyoncore@hotmail.com>
2024-07-05 14:40:51 +0200

    Added IPropriedadesDesbloqueada interface to project.

---
+++ src/Core/Dominio/AcessoDados/IPropriedadesDesbloqueada.cs
@@ -0,0 +1,11 @@
+﻿namespace Snebur.Dominio
+{
+    public interface IPropriedadesDesbloqueada
+    {
+        string NomeEntidade { get; set; }
+
+        string NomeProprieade { get; set; }
+
+        EnumTipoDesbloqueio EnumTipoDesbloqueio { get; set; }
+    }
+}
