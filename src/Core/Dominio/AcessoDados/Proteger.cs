e2114ba56b5d9992957445c5fd7e7811953dceb3
Rubens Cordeiro <zyoncore@hotmail.com>
2024-07-08 17:44:14 +0200

    Refactor: Split classes into separate files
    - Separated classes that were previously in the same file for better organization and maintainability.

--- src/Core/Dominio/AcessoDados/Proteger.cs
+++ src/Core/Dominio/AcessoDados/Proteger.cs
@@ -4,13 +4,7 @@ using System;
 namespace Snebur.Dominio
 {
     [IgnorarAtributoTS]
-    public class NaoAbrirPropriedade : Attribute
-    {
-
-    }
-
-    [IgnorarAtributoTS]
-    public class ProtegerAttribute : Attribute
+    public class Proteger : Attribute
     {
         public bool MostrarConsultaPelaChavePrimaria { get; set; }
 
@@ -19,20 +13,5 @@ namespace Snebur.Dominio
         public int MaximoExibicao { get; set; } = 50;
 
         public TimeSpan EspacoTempo { get; set; } = TimeSpan.FromHours(1);
-    }
-
-    public interface IPropriedadesDesbloqueada
-    {
-        string NomeEntidade { get; set; }
-
-        string NomeProprieade { get; set; }
-
-        EnumTipoDesbloqueio EnumTipoDesbloqueio { get; set; }
-    }
-
-    public enum EnumTipoDesbloqueio
-    {
-        Tudo = 1,
-        UmPorVez = 2,
     }
 }
