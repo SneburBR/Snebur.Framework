a3f6da4e53948b37a3fc7350ecbf4b4c3ea1860a
Rubens Cordeiro <zyoncore@hotmail.com>
2024-08-05 17:03:41 +0200

    Added IsIdentity property to ChavePrimariaAttribute

--- src/Core/Atributos/AcessoDados/ChavePrimaria.cs
+++ src/Core/Atributos/AcessoDados/ChavePrimaria.cs
@@ -5,9 +5,11 @@ namespace Snebur.Dominio.Atributos
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
     public class ChavePrimariaAttribute : BaseAtributoDominio
     {
-        public ChavePrimariaAttribute( )
-        {
+        public bool IsIdentity { get; }
 
+        public ChavePrimariaAttribute(bool isIdentity = true )
+        {
+            this.IsIdentity = isIdentity;
         }
     }
 }
