8db47c32467b8e3afcf1e5cdd65465328973ab48
Rubens Cordeiro <zyoncore@hotmail.com>
2024-08-22 18:28:40 +0200

    Add FieldValueProvider class implementing IValueProvider
    Introduce FieldValueProvider in Snebur.Serializacao namespace.
    - Implements IValueProvider from Newtonsoft.Json.
    - Contains private FieldInfo field.
    - Constructor initializes field with FieldInfo object.
    - SetValue method sets field value on target object.
    - GetValue method retrieves field value from target object.
    - Added using directives for Newtonsoft.Json.Serialization and System.Reflection.

---
+++ src/Core/Serializacao/FieldValueProvider.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Reflection;
+
+namespace Snebur.Serializacao
+{
+    internal class FieldValueProvider : IValueProvider
+    {
+        private FieldInfo field;
+
+        public FieldValueProvider(FieldInfo field)
+        {
+            this.field = field;
+        }
+
+        public void SetValue(object target, object value)
+        {
+            this.field.SetValue(target, value);
+        }
+
+        public object GetValue(object target)
+        {
+            return this.field.GetValue(target);
+        }
+    }
+}
