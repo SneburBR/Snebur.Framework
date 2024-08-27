2d8c316a5e39c6ff67e4c16907c6fb5bd3f3fa60
Rubens Cordeiro <zyoncore@hotmail.com>
2024-08-27 19:02:37 +0200

    Add CacheAttribute class to Snebur.Comunicacao namespace
    A new CacheAttribute class has been introduced in the Snebur.Comunicacao namespace. This class is decorated with the [IgnorarAtributoTS] and [AttributeUsage(AttributeTargets.Method)] attributes, allowing it to be applied to methods. It inherits from System.Attribute and includes a property ExpirarCacheEmMinutos with a public getter and setter. A constructor has been added to initialize this property with an optional parameter. Additionally, using directives for Snebur.Dominio.Atributos and System have been added.

---
+++ src/Comunicacao/src/Comunicacao.Servidor/Atributos/CacheAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Snebur.Dominio.Atributos;
+using System;
+
+namespace Snebur.Comunicacao
+{
+    [IgnorarAtributoTS]
+    [AttributeUsage(AttributeTargets.Method)]
+    public class CacheAttribute : Attribute
+    {
+        public int ExpirarCacheEmMinutos { get; set; }
+
+        public CacheAttribute(int expirarCacheEmMinutos = 0)
+        {
+            this.ExpirarCacheEmMinutos = expirarCacheEmMinutos;
+        }
+    }
+}
