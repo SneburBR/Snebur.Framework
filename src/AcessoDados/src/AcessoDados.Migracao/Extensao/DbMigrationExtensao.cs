65c1cb7cff215e3fa2a11972a64068ef7cafee66
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-01-22 14:11:26 +0100

    Add DbMigrationExtensao class with ColumnExists method
    Introduce DbMigrationExtensao class in Snebur namespace. Add using directives for System.Data.Entity and System.Data.Entity.Migrations. Implement ColumnExists extension method for DbMigration to check for column existence in a specified table using a SQL query executed via DbContext.

---
+++ src/AcessoDados/src/AcessoDados.Migracao/Extensao/DbMigrationExtensao.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace Snebur
+{
+    public static class DbMigrationExtensao
+    {
+        public static bool ColumnExists(this DbMigration dbMigration,
+                                        DbContext dbContext,
+                                        string tableName,
+                                        string columnName)
+        {
+            var sql = $@"SELECT COUNT(*)  FROM INFORMATION_SCHEMA.COLUMNS 
+                            WHERE TABLE_NAME = '{tableName}' 
+                            AND COLUMN_NAME = '{columnName}'";
+
+            return dbContext.Database.SqlQuery<int>(sql).Any();
+        }
+    }
+}
