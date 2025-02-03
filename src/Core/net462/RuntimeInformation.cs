3c34f4cbf793b1da3b0ceccccec4b2b94b03439b
Rubens Cordeiro <rubens.cordeiro@live.com>
2025-02-03 19:31:07 +0100

    Add RuntimeInformation class and OSPlatform struct for net462

---
+++ src/Core/net462/RuntimeInformation.cs
@@ -0,0 +1,31 @@
+﻿#if NET462
+
+namespace System.Runtime.InteropServices
+{
+    public class RuntimeInformation
+    {
+        internal static bool IsOSPlatform(OSPlatform plarform)
+        {
+            return plarform.Equals(OSPlatform.Windows);
+        }
+    }
+
+    public struct OSPlatform : IEquatable<OSPlatform>
+    {
+        private readonly string _osPlatform;
+
+        public OSPlatform(string osPlatform)
+        {
+            this._osPlatform = osPlatform;
+        }
+
+        public bool Equals(OSPlatform other)
+        {
+            return this._osPlatform == other._osPlatform;
+        }
+
+        public static OSPlatform Windows { get; } = new OSPlatform("WINDOWS");
+    }
+}
+ 
+#endif
